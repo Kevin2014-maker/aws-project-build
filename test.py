28936859b7bc7ac907e93d570711742539c80c07
kevin <kevinkamdem@gmail.com>
2026-04-21 15:34:16 +0200

    feat: implementation de la feat say hello

--- test.py
+++ test.py
@@ -5,3 +5,7 @@ from app import say_hello
 class TestApp(unittest.TestCase):
     def test_say_hello(self):
         self.assertEqual(say_hello("AWS"), "Hello, AWS")
+
+
+        if __name__ == "__main__":
+    unittest.main()
